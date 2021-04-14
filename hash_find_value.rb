0fa45d809b6b44a7a725d4406a402ae3fd48ad9d
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-14 00:45:23 +0000

    Solved multiline false problem in hash_find_value

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -9,15 +9,14 @@ sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f =
 p "Enter an integer to find:"
 
 user_integer=gets.chomp
-
+user_integer_found = false
 sample_hash.keys.each do |hashcheck|
-  if sample_hash[hashcheck] == user_integer.to_i
-    found = "#{user_integer} is under the key: #{hashcheck}"
-    p found
+ if sample_hash[hashcheck] == user_integer.to_i
+  user_integer_found = true
+    p "#{user_integer} is under the key: #{hashcheck}"
   end
 end
 
-else
-  not_found = "Could not find the integer #{user_integer}"
-  p not_found
+if user_integer_found == false
+  p "Idiot lol"
 end
